a6164a86187ddf332712c2b0711abb292cdb2f22
Pranab Nag <pkn@pknandita.net>
2024-03-08 12:38:10 -0500

    ch# 2: Added unit of work in all() method to fetch latest and popular movies

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -19,11 +19,28 @@ class MovieDAO:
      If a user_id value is suppled, a `favorite` boolean property should be returned to
      signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::all[]
     def all(self, sort, order, limit=6, skip=0, user_id=None):
-        # TODO: Get list from movies from Neo4j
-        return popular
-    # end::all[]
+        # Define the Unit of Work
+        def get_movies(tx, sort, order, limit, skip, user_id):
+            # Define the cypher statement
+            cypher = """
+                MATCH (m:Movie)
+                WHERE m.`{0}` IS NOT NULL
+                RETURN m {{ .* }} AS movie
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order)
+
+            # Run the statement within the transaction passed as the first argument
+            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id)
+
+            # Extract a list of Movies from the Result
+            return [row.value("movie") for row in result]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_movies, sort, order, limit, skip, user_id)
+
 
     """
     This method should return a paginated list of movies that have a relationship to the
@@ -37,14 +54,12 @@ class MovieDAO:
     If a user_id value is suppled, a `favorite` boolean property should be returned to
     signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::getByGenre[]
     def get_by_genre(self, name, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies in a Genre
         # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
         # MATCH (m:Movie)-[:IN_GENRE]->(:Genre {name: $name})
 
         return popular[skip:limit]
-    # end::getByGenre[]
 
     """
     This method should return a paginated list of movies that have an ACTED_IN relationship
@@ -58,14 +73,12 @@ class MovieDAO:
     If a user_id value is suppled, a `favorite` boolean property should be returned to
     signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::getForActor[]
     def get_for_actor(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies for an Actor
         # TODO: The Cypher string will be formated so remember to escape the braces: {{tmdbId: $id}}
         # MATCH (:Person {tmdbId: $id})-[:ACTED_IN]->(m:Movie)
 
         return popular[skip:limit]
-    # end::getForActor[]
 
     """
     This method should return a paginated list of movies that have an DIRECTED relationship
@@ -79,14 +92,12 @@ class MovieDAO:
     If a user_id value is suppled, a `favorite` boolean property should be returned to
     signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::getForDirector[]
     def get_for_director(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies directed by a Person
         # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
         # MATCH (:Person {tmdbId: $id})-[:DIRECTED]->(m:Movie)
 
         return popular[skip:limit]
-    # end::getForDirector[]
 
     """
     This method find a Movie node with the ID passed as the `id` parameter.
@@ -97,13 +108,11 @@ class MovieDAO:
     If a user_id value is suppled, a `favorite` boolean property should be returned to
     signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::findById[]
     def find_by_id(self, id, user_id=None):
         # TODO: Find a movie by its ID
         # MATCH (m:Movie {tmdbId: $id})
 
         return goodfellas
-    # end::findById[]
 
     """
     This method should return a paginated list of similar movies to the Movie with the
@@ -118,19 +127,15 @@ class MovieDAO:
     If a user_id value is suppled, a `favorite` boolean property should be returned to
     signify whether the user has added the movie to their "My Favorites" list.
     """
-    # tag::getSimilarMovies[]
     def get_similar_movies(self, id, limit=6, skip=0, user_id=None):
         # TODO: Get similar movies from Neo4j
 
         return popular[skip:limit]
-    # end::getSimilarMovies[]
 
 
     """
     This function should return a list of tmdbId properties for the movies that
     the user has added to their 'My Favorites' list.
     """
-    # tag::getUserFavorites[]
     def get_user_favorites(self, tx, user_id):
         return []
-    # end::getUserFavorites[]
