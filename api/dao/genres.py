b0e2d363e367f7ff70361eb1e3080e205763bc88
Pranab Nag <pkn@pknandita.net>
2024-03-10 11:12:03 -0400

    ch#9: Listing genres alphabetically with number of movies and pic of most popular movie in the genre

--- api/dao/genres.py
+++ api/dao/genres.py
@@ -23,15 +23,34 @@ class GenreDAO:
 
     ]
     """
-    # tag::all[]
     def all(self):
-        # TODO: Open a new session
-        # TODO: Define a unit of work to Get a list of Genres
-        # TODO: Execute within a Read Transaction
+        # Define a unit of work to Get a list of Genres
+        def get_movies(tx):
+            result = tx.run("""
+                MATCH (g:Genre)
+                WHERE g.name <> '(no genres listed)'
+                CALL {
+                    WITH g
+                    MATCH (g)<-[:IN_GENRE]-(m:Movie)
+                    WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL
+                    RETURN m.poster AS poster
+                    ORDER BY m.imdbRating DESC LIMIT 1
+                }
+                RETURN g {
+                    .*,
+                    movies: count { (g)<-[:IN_GENRE]-(:Movie) },
+                    poster: poster
+                } AS genre
+                ORDER BY g.name ASC
+            """)
 
-        return genres
-    # end::all[]
+            # index 0 should be genre
+            return [ g.value(0) for g in result ]
 
+        # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+            return session.execute_read(get_movies)
 
     """
     This method should find a Genre node by its name and return a set of properties
@@ -39,11 +58,9 @@ class GenreDAO:
 
     If the genre is not found, a NotFoundError should be thrown.
     """
-    # tag::find[]
     def find(self, name):
         # TODO: Open a new session
         # TODO: Define a unit of work to find the genre by it's name
         # TODO: Execute within a Read Transaction
 
         return [g for g in genres if g["name"] == name][0]
-    # end::find[]
