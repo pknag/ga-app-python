b3eda3b96283c9bc73e70b7cb9a2fbedee68e6cf
Pranab Nag <pkn@pknandita.net>
2024-03-10 12:56:56 -0400

    ch#11: getting movie details by genre, actor and director

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -61,11 +61,28 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     def get_by_genre(self, name, sort='title', order='ASC', limit=6, skip=0, user_id=None):
-        # TODO: Get Movies in a Genre
-        # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
-        # MATCH (m:Movie)-[:IN_GENRE]->(:Genre {name: $name})
+        # Get Movies in a Genre
+        def get_movies_in_genre(tx, sort, order, limit, skip, user_id):
+            favorites = self.get_user_favorites(tx, user_id)
 
-        return popular[skip:limit]
+            cypher = """
+                MATCH (m:Movie)-[:IN_GENRE]->(:Genre {{name: $name}})
+                WHERE m.`{0}` IS NOT NULL
+                RETURN m {{
+                    .*,
+                    favorite: m.tmdbId in $favorites
+                }} AS movie
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, name=name, limit=limit, skip=skip, user_id=user_id, favorites=favorites)
+
+            return [ row.get("movie") for row in result ]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_movies_in_genre, sort, order, limit=limit, skip=skip, user_id=user_id)
 
     """
     This method should return a paginated list of movies that have an ACTED_IN relationship
@@ -80,11 +97,28 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     def get_for_actor(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
-        # TODO: Get Movies for an Actor
-        # TODO: The Cypher string will be formated so remember to escape the braces: {{tmdbId: $id}}
-        # MATCH (:Person {tmdbId: $id})-[:ACTED_IN]->(m:Movie)
+        # Get Movies for an Actor
+        def get_movies_for_actor(tx, id, sort, order, limit, skip, user_id):
+            favorites = self.get_user_favorites(tx, user_id)
 
-        return popular[skip:limit]
+            cypher = """
+                MATCH (:Person {{tmdbId: $id}})-[:ACTED_IN]->(m:Movie)
+                WHERE m.`{0}` IS NOT NULL
+                RETURN m {{
+                    .*,
+                    favorite: m.tmdbId in $favorites
+                }} AS movie
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, id=id, limit=limit, skip=skip, user_id=user_id, favorites=favorites)
+
+            return [ row.get("movie") for row in result ]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_movies_for_actor, id, sort, order, limit=limit, skip=skip, user_id=user_id)
 
     """
     This method should return a paginated list of movies that have an DIRECTED relationship
@@ -99,11 +133,28 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     def get_for_director(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
-        # TODO: Get Movies directed by a Person
-        # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
-        # MATCH (:Person {tmdbId: $id})-[:DIRECTED]->(m:Movie)
+        # Get Movies directed by a Person
+        def get_movies_for_director(tx, id, sort, order, limit, skip, user_id):
+            favorites = self.get_user_favorites(tx, user_id)
 
-        return popular[skip:limit]
+            cypher = """
+                MATCH (:Person {{tmdbId: $id}})-[:DIRECTED]->(m:Movie)
+                WHERE m.`{0}` IS NOT NULL
+                RETURN m {{
+                    .*,
+                    favorite: m.tmdbId in $favorites
+                }} AS movie
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, id=id, limit=limit, skip=skip, user_id=user_id, favorites=favorites)
+
+            return [ row.get("movie") for row in result ]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_movies_for_director, id, sort, order, limit=limit, skip=skip, user_id=user_id)
 
     """
     This method find a Movie node with the ID passed as the `id` parameter.
